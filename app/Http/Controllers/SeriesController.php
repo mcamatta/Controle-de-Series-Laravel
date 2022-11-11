2202bdad78591c3a2a331778addb9f8efdc6671f
mcamatta <camattamichel@gmail.com>
2022-11-10 23:13:17 -0300

    + Deletando séries e exibindo mensagens via sessão

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -8,11 +8,13 @@ use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index() {
-       
+    public function index(Request $request) 
+    {   
         $series = Serie::query()->orderBy('nome')->get();
+        $mensagemSucesso = $request->session()->get('mensagem.sucesso');
 
-        return view('series.index')->with('series', $series);
+        return view('series.index')
+            ->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
     }
 
     public function create() {
@@ -21,7 +23,16 @@ class SeriesController extends Controller
 
     public function store(Request $request) 
     {
-        Serie::create($request->all());
+        $serie = Serie::create($request->all());
+        $request->session()->flash('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+
+        return to_route('series.index');
+    }
+
+    public function destroy(Serie $series, Request $request) 
+    {
+        $series->delete();
+        $request->session()->flash('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
 
         return to_route('series.index');
     }
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,10 +1,26 @@
 <x-layout title="Séries">
 
     <a href="{{ route('series.create') }}" class="btn btn-dark mb-3">Adicionar</a>
+    
+    @isset($mensagemSucesso)
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{$mensagemSucesso}}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endisset
 
     <ul class="list-group">
         @foreach ($series as $serie)
-            <li class="list-group-item">{{ $serie->nome }}</li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                {{ $serie->nome }}
+                <form action="{{ route('series.destroy', $serie->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-sm">
+                        X
+                    </button>
+                </form>
+            </li>
         @endforeach
     </ul>
 </x-layout>
