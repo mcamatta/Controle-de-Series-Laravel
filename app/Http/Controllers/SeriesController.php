8cb9c0fb46cf20c1be11b735f21ce22057549460
mcamatta <camattamichel@gmail.com>
2022-11-26 22:35:59 -0300

    + Adicionado constraint nos episódios e temporada +  Salvando temporadas e episódios na series controller

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\Serie;
+use App\Models\Episode;
+use App\Models\Season;
+use App\Models\Series;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
     public function index(Request $request) 
     {   
-        $series = Serie::all();
+        $series = Series::all();
         $mensagemSucesso = $request->session()->get('mensagem.sucesso');
 
         return view('series.index')
@@ -23,13 +25,33 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request) 
     {
-        $serie = Serie::create($request->all());
+        $serie = Series::create($request->all());
+        
+        $seasons = [];
+        for($i = 1; $i <= $request->seasonsQty; $i++) {
+            $seasons[] = [
+                'series_id' => $serie->id,
+                'number' => $i
+            ];
+        }
+        Season::insert($seasons);
 
+        $episodes = [];
+        foreach($serie->seasons as $season) {
+            for($i = 1; $i <= $request->episodesPerSeasons; $i++) {
+                $episodes[] = [
+                    'season_id' => $season->id,
+                    'number' => $i
+                ];
+            }
+        }
+        Episode::insert($episodes);
+        
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
     }
 
-    public function destroy(Serie $series) 
+    public function destroy(Series $series) 
     {
         $series->delete();
 
@@ -37,12 +59,12 @@ class SeriesController extends Controller
             ->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
     }
 
-    public function edit(Serie $series) {
+    public function edit(Series $series) {
         return view('series.edit')
             ->with('series', $series);
     }
 
-    public function update(Serie $series, SeriesFormRequest $request) {
+    public function update(Series $series, SeriesFormRequest $request) {
         $series->fill($request->all());
         $series->save();
 
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,3 +1,36 @@
 <x-layout title="Nova Série">
-    <x-series.form :action="route('series.store')" :nome="old('nome')" :update="false"/>
+    <form action="{{ route('series.store') }}" method="post">
+        @csrf
+        <div class="row mb-3">
+            <div class="col-8">
+                <label for="nome" class="form-label">Nome:</label>
+                <input 
+                    autofocus
+                    type="text" 
+                    class="form-control" 
+                    id="nome" 
+                    name="nome"
+                    value="{{ old('nome') }}">
+            </div>
+            <div class="col-2">
+                <label for="seasonsQty" class="form-label">Nº temporadas:</label>
+                <input 
+                    type="text" 
+                    class="form-control" 
+                    id="seasonsQty" 
+                    name="seasonsQty"
+                    value="{{ old('seasonsQty') }}">
+            </div>
+            <div class="col-2">
+                <label for="episodesPerSeasons" class="form-label">Eps / Temporada:</label>
+                <input 
+                    type="text" 
+                    class="form-control" 
+                    id="episodesPerSeasons" 
+                    name="episodesPerSeasons"
+                    value="{{ old('episodesPerSeasons') }}">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+    </form>
 </x-layout>
